770d2b85a723ccb3bd75e95e039b15bc46ec2419
Viljen <viljen.vassbo@gmail.com>
2025-12-30 23:49:46 +0100

    Start day 10 in ts

---
+++ 2025/Day11.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::cout << "Hello from C++" << std::endl;
+  return 0;
+}
--- 2025/template.ts
+++ 2025/template.ts
@@ -1,1 +1,24 @@
-console.log("Hello from TypeScript");
+import {readFileSync} from "fs";
+interface Entries {
+    lights: string
+    buttons: number[][]
+    joltage: number[]
+}
+
+let entries: Entries = {
+    lights: "",
+    buttons: [],
+    joltage: []
+}
+
+function readFile(): void {
+    const file = readFileSync("Day10.txt", "utf-8");
+    const lines = file.split("\n");
+    const lights = lines.map((line) => line);
+    const buttons = lines.map((line) => line.split(""));
+    const joltage = lines.map((line) => parseInt(line));
+    entries = { lights, buttons, joltage };
+}
+
+readFile();
+console.log(entries);
