90690adc489a1aec9f7e4dcdb8a1114693f038c0
Viljen <viljen.vassbo@gmail.com>
2026-01-05 00:01:18 +0100

    Day 12 p1

--- 2025/Day12.py
+++ 2025/Day12.py
@@ -1,1 +1,31 @@
-lines = []
+boxes = []
+sizes = []
+boxesToFit = []
+
+with open("Day12.txt", "r") as file:
+    line = file.readline()
+    while line:
+        line = line.strip()
+        if len(line) == 2:
+            box = [file.readline().strip() for _ in range(3)]
+            boxes.append(box)
+            sizes.append(sum([b.count("#") for b in box]))
+        if len(line) > 3:
+            line = line.split(" ")
+            boxesToFit.append([line[0][:-1],[int(i) for i in line[1:]]])
+        line = file.readline()
+
+print(boxes)
+print(sizes)
+print(boxesToFit)
+tot = 0
+for [dimentions, boxes] in boxesToFit:
+    dimsList = dimentions.split("x")
+    dims = int(dimsList[0])*int(dimsList[1])
+    filled = 0
+    for (idx, val) in enumerate(boxes):
+        filled += sizes[idx]*val
+    if(filled<=dims):tot+=1
+        
+print(tot)
+    
